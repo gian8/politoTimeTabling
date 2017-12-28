de05d071be2b24eadf7782001d041c5b898393ad
gianluca mangiapelo <gianluca.mangiapelo@deltatre.com>
2017-12-28 11:44:21 +0100

    first commit for project nb

---
+++ polito-timeTabling/src/polito/timetabling/PolitoTimeTabling.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polito.timetabling;
+
+/**
+ *
+ * @author gianluca.mangiapelo
+ */
+public class PolitoTimeTabling {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Polito Time Tabling First commit");
+    }
+    
+}
