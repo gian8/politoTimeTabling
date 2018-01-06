1bb2ab4214439570db9534600961d3e17b933d62
gianluca mangiapelo <gianluca.mangiapelo@deltatre.com>
2018-01-06 13:41:24 +0100

    Add to suddivide exam in Slot time

---
+++ polito-timeTabling/src/polito/timetabling/Slot.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2018 gianluca.mangiapelo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package polito.timetabling;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author gianluca.mangiapelo
+ */
+public class Slot {
+    private HashMap<Integer,List<Integer>> timeSlot;
+    private int maxTimeSlot;
+    private List<Integer> listExams;
+    
+    public Slot(int maxTimeSlot,List<Integer> listExams){
+        this.listExams = listExams;
+        this.maxTimeSlot = maxTimeSlot;
+        this.timeSlot = new HashMap<>(this.maxTimeSlot);
+        this.setExamInSlot();
+    }
+    
+    private void setExamInSlot(){
+        
+        if(this.listExams.size()>this.maxTimeSlot){
+        
+        }
+        else{
+            for(int i = 0; i < this.listExams.size(); i++){
+                List<Integer> listSingleExams = new ArrayList<>();
+                listSingleExams.add(listExams.get(i));
+                this.timeSlot.put(i, listSingleExams);
+            }
+        }
+        
+    }
+    
+    public HashMap<Integer,List<Integer>> getExamInSlot(){
+        return this.timeSlot;
+    }
+    
+}
